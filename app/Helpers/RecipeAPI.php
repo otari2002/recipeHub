588cc9ff92b73edf5146e6ea2b1830f2c35f06a1
otari2002 <omar.ibn.tahri@gmail.com>
2024-04-11 00:22:38 +0000

    slight change until correction

--- app/Helpers/RecipeAPI.php
+++ app/Helpers/RecipeAPI.php
@@ -6,14 +6,14 @@ use Illuminate\Support\Facades\Http;
 
 class RecipeAPI
 {
-    static private $apiKey;
+    private static $apiKey;
     public static function init()
     {
         self::$apiKey = 'dada6460d0d04992ba1294662629d53e';
     }
 
     public static function randomRecipes($num=10){
-        $recipes = Http::withHeader('x-api-key',self::$apiKey)
+        $recipes = Http::withHeader('x-api-key','dada6460d0d04992ba1294662629d53e')
         ->get('https://api.spoonacular.com/recipes/random',
         [
             'limitLicense' => true,
