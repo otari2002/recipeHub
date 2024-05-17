f92263a6bf27f03997b509069178f93ee49151c3
otari2002 <omar.ibn.tahri@gmail.com>
2024-05-17 11:34:17 +0100

    removed what wont be used

--- routes/api.php
+++ routes/api.php
@@ -21,17 +21,6 @@ Route::get('test', [RegisterController::class, 'index']);
 // User Login (email, password)
 Route::post('login', [LoginController::class, 'login']);
 
-// Social Login (token)
-Route::post('auth/google', [SocialiteController::class, 'requestTokenGoogle']);
-Route::post('auth/facebook', [SocialiteController::class, 'requestTokenFacebook']);
-
-// Login via OTP
-// Sending otp code
-Route::post('login/email', [LoginOtpController::class, 'requestEmail']); // (email)
-
-// Verify the code and login (code, email)
-Route::post('login/verify-otp', [LoginOtpController::class, 'processLogin']);
-
 //Password Reset
 // Sending otp code
 Route::post('forgot-password/email', [ResetPasswordController::class, 'requestEmail']); // (email)
@@ -61,21 +50,12 @@ Route::middleware([EnsureUserLoggedIn::class])->group(function () {
     Route::post('recipes/save', [RecipeController::class, 'saveRecipe']);
     // Remove a recipefrom saved recipes (idRecipe)
     Route::post('recipes/unsave', [RecipeController::class, 'unsaveRecipe']);
-    // Like a recipe(idRecipe)
-    Route::post('recipes/like', [RecipeController::class, 'likeRecipe']);
-    // Get Recipe with its ID
-    Route::post('post', [RecipeController::class, 'getRecipe']);
-    // Remove Like from a recipe(idRecipe)
-    // Route::post('recipes/unlike', [RecipeController::class, 'unlikeRecipe']);
-
-    // Create a comment (idRecipe, commentText)
-    Route::post('comments/create', [CommentController::class, 'createComment']);
-    // Delete a comment (idComment)
-    Route::post('comments/delete', [CommentController::class, 'deleteComment']);
-
+    
     // Get current user
     Route::get('me', [UserController::class, 'getCurrentUser']);
 
+    // Get current user's saved recipes
+    Route::get('saved-recipes', [UserController::class, 'getSavedRecipes']);
     // Change personnal data (fullName?, username?, email?)
     Route::post('changePersonnalData', [UserController::class, 'changePersonnalData']);
 
